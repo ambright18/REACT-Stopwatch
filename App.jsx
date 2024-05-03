b9a1029a27aa7fc31c649b6ac7f6a1d6855735a4
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-05-03 12:22:39 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Stopwatch from "./components/Stop_Watch/Stopwatch";
+function App() {
+    return (
+    <>
+    <Stopwatch />
+    </>
+    );
+}
+
+export default App;
